dd43f9dcf3c53267812e0bafe82353f7cd38f600
AOKI Hanae <aereal@kerare.org>
2011-10-29 06:47:13 +0900

    add hook system

--- kaminari/lib/kaminari.rb
+++ kaminari/lib/kaminari.rb
@@ -2,3 +2,15 @@ require "kaminari/config"
 require "kaminari/helpers/action_view_extension"
 require "kaminari/helpers/paginator"
 require "kaminari/models/array_extension"
+
+module Kaminari
+  module Initializer
+    def self.register(name)
+      (@hooks ||= []) << name.intern
+    end
+
+    def self.init!
+      @hooks.each {|hook| __send__ hook }
+    end
+  end
+end
